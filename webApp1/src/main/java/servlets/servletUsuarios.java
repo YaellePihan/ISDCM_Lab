f40539ad7fc74fa7727c1fb212fd9d4e25976f68
isaaccalvis <isaaccalvis@gmail.com>
2022-03-14 20:00:08 +0100

    All text_labels from Register page now are mandatory.

--- webApp1/src/main/java/servlets/servletUsuarios.java
+++ webApp1/src/main/java/servlets/servletUsuarios.java
@@ -50,48 +50,57 @@ public class servletUsuarios extends HttpServlet {
         PrintWriter pwriter = res.getWriter();
         String syst_msg_toRegisterJSP = "";
 
-        if (user_pass1.compareTo(user_pass2) == 0 && !user_pass1.isEmpty())
+        if (user_name.isEmpty() || user_surnames.isEmpty() || user_email.isEmpty() || user_nick.isEmpty() || user_pass1.isEmpty() || user_pass2.isEmpty())
         {
-            if (!CheckIfEMailExist(user_email))
+            RequestDispatcher reqDisp = req.getRequestDispatcher("registroUsu.jsp");
+            req.setAttribute("SYST_MESSAGE", "System message: Any field with (*) can be empty!");
+            reqDisp.forward(req, res);
+        }
+        else
+        {
+            if (user_pass1.compareTo(user_pass2) == 0 && !user_pass1.isEmpty())
             {
-                usuario user = new usuario();
-                // Check if user is also created
-                if (!CheckIfUserExist(user, user_nick,user_pass1))
+                if (!CheckIfEMailExist(user_email))
                 {
-                    // Create user class
-                    user.setNombre(user_name);
-                    user.setApellidos(user_surnames);
-                    user.setApodo(user_nick);
-                    user.setContrasena(user_pass1);
-                    user.setEmail(user_email);
-                    
-                    // Add to the DataBase
-                    String result = user.add_user_to_db();
-                    // If user succesfully created:
-                    RequestDispatcher reqDisp = req.getRequestDispatcher("login.jsp");
-                    req.setAttribute("SYST_MESSAGE", "Created succesfully :), now can login!");
-                    reqDisp.forward(req, res);
+                    usuario user = new usuario();
+                    // Check if user is also created
+                    if (!CheckIfUserExist(user, user_nick,user_pass1))
+                    {
+                        // Create user class
+                        user.setNombre(user_name);
+                        user.setApellidos(user_surnames);
+                        user.setApodo(user_nick);
+                        user.setContrasena(user_pass1);
+                        user.setEmail(user_email);
+
+                        // Add to the DataBase
+                        String result = user.add_user_to_db();
+                        // If user succesfully created:
+                        RequestDispatcher reqDisp = req.getRequestDispatcher("login.jsp");
+                        req.setAttribute("SYST_MESSAGE", "Created succesfully :), now can login!");
+                        reqDisp.forward(req, res);
+                    }
+                    else
+                    {
+                        syst_msg_toRegisterJSP = "System message: This user nickname already exsits !!!";
+                    }
                 }
                 else
                 {
-                    syst_msg_toRegisterJSP = "System message: This user nickname already exsits !!!";
+                    syst_msg_toRegisterJSP = "System message: This user email already is used !!!";
                 }
             }
             else
             {
-                syst_msg_toRegisterJSP = "System message: This user email already is used !!!";
+                syst_msg_toRegisterJSP = "System message: Password1 != Password2 or Password = Null !!!";
             }
-        }
-        else
-        {
-            syst_msg_toRegisterJSP = "System message: Password1 != Password2 or Password = Null !!!";
-        }
-        
-        if (!syst_msg_toRegisterJSP.isEmpty())
-        {
-            RequestDispatcher reqDisp = req.getRequestDispatcher("registroUsu.jsp");
-            req.setAttribute("SYST_MESSAGE", syst_msg_toRegisterJSP);
-            reqDisp.forward(req, res);
+
+            if (!syst_msg_toRegisterJSP.isEmpty())
+            {
+                RequestDispatcher reqDisp = req.getRequestDispatcher("registroUsu.jsp");
+                req.setAttribute("SYST_MESSAGE", syst_msg_toRegisterJSP);
+                reqDisp.forward(req, res);
+            }
         }
     }
     
