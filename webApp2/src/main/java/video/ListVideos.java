b5d3535923a887d0776b9db7109c4d44ce325d4b
YaellePihan <yaelle.pihan@gmail.com>
2022-03-22 15:02:46 +0100

    creacion de webapp2 con rest: hay el nuevo servletREST y una copia del video package

---
+++ webApp2/src/main/java/video/ListVideos.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.ArrayList;
+/**
+ *
+ * @author alumne
+ */
+
+public class ListVideos{
+    //Constructor   
+    public ListVideos() {
+    }
+    
+    //to get all videos registered in database
+    public ArrayList<video> getList_videos() {
+        ArrayList<video> list_videos = new ArrayList();
+        String query = "select * from videos";
+        Connection c = null;
+        try{
+            c = DriverManager.getConnection("jdbc:derby://localhost:1527/isdcm_lab_db;user=isdcm_lab_db;password=isdcm_lab_db");
+            PreparedStatement statement;
+            statement = c.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            int counter = 0;
+            while(rs.next()){
+                counter +=1;
+                String title = rs.getString("TITLE");
+                String author = rs.getString("AUTHOR");
+                String creation_date = rs.getString("UPDATE_DATE");
+                String time = rs.getString("TIME");
+                String description = rs.getString("DESCRIPTION");
+                String format = rs.getString("FORMAT");
+                int nb = rs.getInt("NB_OF_REPRODUCTIONS");
+                int id = rs.getInt("ID");                       
+                video video = new video(title, author, creation_date, time, description, format,nb);
+                video.setId(id);
+                list_videos.add(video);
+            }
+        }catch (SQLException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        } finally {
+            try {
+                if (c != null) 
+                    c.close();                
+            } catch (SQLException e) {
+                System.out.println(Arrays.toString(e.getStackTrace()));
+            }
+        }
+        return list_videos;
+    }
+}
---
+++ webApp2/src/main/java/video/video.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+/**
+ *
+ * @author alumne
+ */
+public class video {
+    String titulo;
+    String autor;
+    String fecha_creacion;
+    String duracion;
+    int num_reproduccion;
+    int id;
+    String descripcion;
+    String formato;
+
+    //constructor 1 
+    public video(){};
+    
+    //constructor 2
+    public video(String title, String author, String date, String time, String description, String format, int nb){
+        this.titulo = title;
+        this.autor = author;
+        this.fecha_creacion = date;
+        this.duracion = time;
+        this.descripcion = description;
+        this.formato = format;
+        this.num_reproduccion = nb;
+    }
+   
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setFecha_creacion(String fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public void setNum_reproduccion(int num_reproduccion) {
+        this.num_reproduccion = num_reproduccion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+    
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public int getNum_reproduccion() {
+        return num_reproduccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+    
+    
+    // to register a video in the database
+    public String add_video_to_db(){
+        String result = "Error when adding video to database";
+        // define query
+        String query = "insert into videos(\"TITLE\",\"AUTHOR\",\"UPDATE_DATE\",\"TIME\",\"NB_OF_REPRODUCTIONS\",\"DESCRIPTION\",\"FORMAT\") values(?,?,?,?,?,?,?)";
+        Connection c = null;
+        try{
+            // create a database connection
+            result ="enter into try";
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            result="before connection";            
+            c = DriverManager.getConnection("jdbc:derby://localhost:1527/isdcm_lab_db;user=isdcm_lab_db;password=isdcm_lab_db");
+            
+            // prepare statement
+            PreparedStatement statement;
+            statement = c.prepareStatement(query);
+            statement.setString(1,this.getTitulo());
+            statement.setString(2,this.getAutor());
+            statement.setString(3,this.getFecha_creacion());
+            statement.setString(4,this.getDuracion());
+            statement.setInt(5,this.getNum_reproduccion());
+            statement.setString(6,this.getDescripcion());
+            statement.setString(7,this.getFormato());
+            
+            // execute query
+            int i = statement.executeUpdate();
+            result = "Video successfully added to database";
+        }catch (ClassNotFoundException | SQLException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }finally {
+            try {
+                if (c != null) 
+                    c.close();                
+            } catch (SQLException e) {
+                System.out.println(Arrays.toString(e.getStackTrace()));
+            }
+        }
+        return result;
+    }
+    
+    
+    //to check if a video already exist in the db
+    public Boolean is_video_in_db(){
+        Boolean result = true;
+        String query = "select count(*) from videos where title=?";
+        Connection c = null;
+        try{
+            // create connection to db
+            c = DriverManager.getConnection("jdbc:derby://localhost:1527/isdcm_lab_db;user=isdcm_lab_db;password=isdcm_lab_db");
+            
+            // prepare statement
+            PreparedStatement statement;
+            statement = c.prepareStatement(query);
+            statement.setString(1, this.getTitulo());
+            
+            // execute query and analyse results
+            ResultSet r = statement.executeQuery();
+        if (r.next())
+            {
+                if (r.getInt(1) == 0) //if the number of video == 0
+                    result = false;   //then video does not exist yet
+            }    
+        }catch (SQLException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        } finally {
+            try {
+                if (c != null) 
+                    c.close();                
+            } catch (SQLException e) {
+                System.out.println(Arrays.toString(e.getStackTrace()));
+            }
+        }
+        return result;
+    }
+    
+}
