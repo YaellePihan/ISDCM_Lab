755fc6a71efbf6cf891555045b27690aecb0290c
isaaccalvis <isaaccalvis@gmail.com>
2022-04-06 22:26:23 +0200

    Check if date is correct! works

--- webApp1/src/main/java/servlets/servletREST.java
+++ webApp1/src/main/java/servlets/servletREST.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import javax.servlet.RequestDispatcher;
+import javax.servlet.RequestDispatcher;
 //import java.io.OutputStream;
 
 import java.net.URL;
@@ -20,6 +20,7 @@ import java.io.InputStreamReader;
 
 @WebServlet(name = "servletREST", urlPatterns = {"/servletREST"})
 public class servletREST extends HttpServlet {
+    
     protected void processRequestGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -31,39 +32,80 @@ public class servletREST extends HttpServlet {
             String search_date_d    = request.getParameter("video_search_date_day");
             String search_date_m    = request.getParameter("video_search_date_month");
             String search_date_y    = request.getParameter("video_search_date_year");
+            
+            if (!CheckDateValues(search_date_d, search_date_m, search_date_y))
+            {
+                // TODO: Fix that!
+                RequestDispatcher reqDisp = request.getRequestDispatcher("listadoVid.jsp");
+                request.setAttribute("SYST_MESSAGE", "Invalid date!");
+                reqDisp.forward(request, response);
+            }
+            else
+            {
+               // Generate URL
+               String urlToAsk = "http://localhost:8080/webApp2/resources/javaee8/searchVideo?";
+               String requestParameters = "title=" + search_title + "&author=" + search_author + "&date_d=" + search_date_d + "&date_m=" + search_date_m + "&date_y=" + search_date_y;
+               urlToAsk += requestParameters;
 
-            // Generate URL
-            String urlToAsk = "http://localhost:8080/webApp2/resources/javaee8/searchVideo?";
-            String requestParameters = "title=" + search_title + "&author=" + search_author + "&date_d=" + search_date_d + "&date_m=" + search_date_m + "&date_y=" + search_date_y;
-            urlToAsk += requestParameters;
-            
-            // Create connection
-            URL getVideoListURL = new URL(urlToAsk);
-            HttpURLConnection http_connection = (HttpURLConnection)getVideoListURL.openConnection();
-            http_connection.setRequestMethod("GET");
-            http_connection.setRequestProperty("Accept", "text/plain");
-            
-            if(http_connection.getResponseCode() != 200)
-            {
-                // TODO: Add error message
+               // Create connection
+               URL getVideoListURL = new URL(urlToAsk);
+               HttpURLConnection http_connection = (HttpURLConnection)getVideoListURL.openConnection();
+               http_connection.setRequestMethod("GET");
+               http_connection.setRequestProperty("Accept", "text/plain");
+
+               if(http_connection.getResponseCode() != 200)
+               {
+                   // TODO: Add error message
+               }
+
+               // Get Result
+               BufferedReader read = new BufferedReader(new InputStreamReader(http_connection.getInputStream(), "utf-8"));
+               String outputPart = read.readLine();
+               String finalOutput = null;
+               while(outputPart != null)
+               {
+                   finalOutput += outputPart + "<br>";
+                   outputPart = read.readLine();
+               }
+
+               // tmp: print results
+               out.println("<!DOCTYPE html>");
+               out.println("<html><head></head><body>");
+               out.println("." + search_date_d + ".");
+               out.println(finalOutput);
+               out.println("</body></html>");   
             }
-            
-            // Get Result
-            BufferedReader read = new BufferedReader(new InputStreamReader(http_connection.getInputStream(), "utf-8"));
-            String outputPart = read.readLine();
-            String finalOutput = null;
-            while(outputPart != null)
-            {
-                finalOutput += outputPart + "<br>";
-                outputPart = read.readLine();
-            }
-            
-            // tmp: print results
-            out.println("<!DOCTYPE html>");
-            out.println("<html><head></head><body>");
-            out.println(finalOutput);
-            out.println("</body></html>");
         }
+    }
+    
+    boolean CheckDateValues(String d, String m, String y)
+    {
+        boolean ret = true;
+        
+        if (!(d.compareTo("1") == 0 || d.compareTo("2") == 0 || d.compareTo("3") == 0 || d.compareTo("4") == 0 || d.compareTo("5") == 0 || d.compareTo("6") == 0
+                 || d.compareTo("7") == 0 || d.compareTo("8") == 0 || d.compareTo("9") == 0 || d.compareTo("10") == 0 || d.compareTo("11") == 0 || d.compareTo("12") == 0
+                 || d.compareTo("13") == 0 || d.compareTo("14") == 0 || d.compareTo("15") == 0 || d.compareTo("16") == 0 || d.compareTo("17") == 0 || d.compareTo("18") == 0
+                 || d.compareTo("19") == 0 || d.compareTo("20") == 0 || d.compareTo("21") == 0 || d.compareTo("22") == 0 || d.compareTo("23") == 0 || d.compareTo("24") == 0
+                 || d.compareTo("25") == 0 || d.compareTo("26") == 0 || d.compareTo("27") == 0 || d.compareTo("28") == 0 || d.compareTo("29") == 0 || d.compareTo("30") == 0
+                 || d.compareTo("31") == 0))
+        {
+            ret = false;
+        }
+        
+        if (!(m.compareTo("1") == 0 || m.compareTo("2") == 0 || m.compareTo("3") == 0 || m.compareTo("4") == 0 || m.compareTo("5") == 0 || m.compareTo("6") == 0 || 
+                m.compareTo("7") == 0 || m.compareTo("8") == 0 || m.compareTo("9") == 0 || m.compareTo("10") == 0 || m.compareTo("11") == 0 || m.compareTo("12") == 0))
+        {
+            ret = false;
+        }
+        
+        try{
+            Integer.parseInt(y);
+        }catch(NumberFormatException e)
+        {
+            ret = false;
+        }
+        
+        return ret;
     }
     
     protected void processRequestPost(HttpServletRequest request, HttpServletResponse response)
